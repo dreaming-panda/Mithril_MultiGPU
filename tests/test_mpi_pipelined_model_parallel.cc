4b796511b1922ef52d6010ab2b07359725f4efbe
dreaming-panda <64370186+dreaming-panda@users.noreply.github.com>
2022-07-12 19:02:06 +0800

    Update test_mpi_pipelined_model_parallel.cc

--- tests/test_mpi_pipelined_model_parallel.cc
+++ tests/test_mpi_pipelined_model_parallel.cc
@@ -87,7 +87,7 @@ int main(int argc, char ** argv) {
     });*/
 
 
-    std::string graph_path = "/home/a100/storage/gnn_datasets/arxiv";
+    std::string graph_path = "/home/p100/storage/gnn_datasets/arxiv";
     int num_layers = 2;
     int num_hidden_units = 128;
     int num_epoch = 3000;
