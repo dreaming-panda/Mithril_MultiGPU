f051eed0a930d25e5523635d9a9cd467d69d82a0
richardjcy001 <richardjiang01@outlook.com>
2022-11-02 16:02:13 -0500

    added new results

--- results/test_acc/async_multi_gpus/run.sh
+++ results/test_acc/async_multi_gpus/run.sh
@@ -27,8 +27,8 @@ export OMP_NUM_THREADS=12
 
 # Launch MPI code... 
 
-#for dataset in cora citeseer pubmed ogbn-arxiv
-for dataset in ogbn-arxiv
+for dataset in cora citeseer pubmed ogbn-arxiv
+#for dataset in ogbn-arxiv
 do
     ibrun ./build/applications/async_multi_gpus/gcn --graph /work/03924/xuehaiq/maverick2/jingji/gnn_datasets/$dataset --layers 3 --hunits 128 --epoch 5000 --lr 0.001 --decay 0 --part hybrid | tee ./results/test_acc/async_multi_gpus/${dataset}_gcn.txt 
 done
