7a39b4fed73ee591ebb5dc728e1a4839662e56be
Amadeus Chan <greenclouds@foxmail.com>
2022-12-07 00:39:03 -0500

    added new data

--- run_anvil.sh
+++ run_anvil.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -p gpu 
 #SBATCH -A cis220117-gpu 
-#SBATCH -t 01:00:00 
+#SBATCH -t 03:00:00 
 #SBATCH --gpus-per-node 1
 #SBATCH --nodes 1
 #SBATCH --ntasks-per-node 1 
@@ -14,6 +14,18 @@ module list
 cd build 
 make -j
 
+#mpirun --map-by node:PE=$SLURM_CPUS_PER_TASK ./applications/single_gpu/gcn --graph $PROJECT/gnn_datasets/reordered/reddit --layers 3 --hunits 256 --epoch 5000 --lr 1e-3 --decay 0 | tee ../results/analyze_version_matching_and_weight_prediction/3_layer/sync/reddit_gcn.txt
+#mpirun --map-by node:PE=$SLURM_CPUS_PER_TASK ./applications/single_gpu/gcn --graph $PROJECT/gnn_datasets/reordered/ogbn_products --layers 3 --hunits 256 --epoch 5000 --lr 1e-3 --decay 0 | tee ../results/analyze_version_matching_and_weight_prediction/3_layer/sync/products_gcn.txt
+#mpirun --map-by node:PE=$SLURM_CPUS_PER_TASK ./applications/single_gpu/gcn --graph $PROJECT/gnn_datasets/reordered/ogbn_arxiv --layers 3 --hunits 256 --epoch 5000 --lr 1e-3 --decay 0 | tee ../results/analyze_version_matching_and_weight_prediction/3_layer/sync/arxiv_gcn.txt
+
+#mpirun --map-by node:PE=$SLURM_CPUS_PER_TASK ./applications/async_multi_gpus/gcn --graph $PROJECT/gnn_datasets/reordered/reddit --layers 3 --hunits 256 --epoch 5000 --lr 1e-3 --decay 0 | tee ../results/analyze_version_matching_and_weight_prediction/3_layer/async/reddit_gcn.txt
+#mpirun --map-by node:PE=$SLURM_CPUS_PER_TASK ./applications/async_multi_gpus/gcn --graph $PROJECT/gnn_datasets/reordered/ogbn_products --layers 3 --hunits 256 --epoch 5000 --lr 1e-3 --decay 0 | tee ../results/analyze_version_matching_and_weight_prediction/3_layer/async/products_gcn.txt
+mpirun --map-by node:PE=$SLURM_CPUS_PER_TASK ./applications/async_multi_gpus/gcn --graph $PROJECT/gnn_datasets/reordered/ogbn_arxiv --layers 3 --hunits 256 --epoch 5000 --lr 1e-3 --decay 0 | tee ../results/analyze_version_matching_and_weight_prediction/3_layer/async/arxiv_gcn.txt
+
+mpirun --map-by node:PE=$SLURM_CPUS_PER_TASK ./applications/single_gpu/gcn --graph $PROJECT/gnn_datasets/reordered/reddit --layers 3 --hunits 256 --epoch 5000 --lr 1e-3 --decay 0 | tee ../results/analyze_version_matching_and_weight_prediction/3_layer/async_pred/reddit_gcn.txt
+mpirun --map-by node:PE=$SLURM_CPUS_PER_TASK ./applications/single_gpu/gcn --graph $PROJECT/gnn_datasets/reordered/ogbn_products --layers 3 --hunits 256 --epoch 5000 --lr 1e-3 --decay 0 | tee ../results/analyze_version_matching_and_weight_prediction/3_layer/async_pred/products_gcn.txt
+mpirun --map-by node:PE=$SLURM_CPUS_PER_TASK ./applications/single_gpu/gcn --graph $PROJECT/gnn_datasets/reordered/ogbn_arxiv --layers 3 --hunits 256 --epoch 5000 --lr 1e-3 --decay 0 | tee ../results/analyze_version_matching_and_weight_prediction/3_layer/async_pred/arxiv_gcn.txt
+
 ### run single-node without async
 #mpirun --map-by node:PE=$SLURM_CPUS_PER_TASK ./applications/single_gpu/gcn --graph $PROJECT/gnn_datasets/reordered/reddit --layers 2 --hunits 256 --epoch 1000 --lr 1e-3 --decay 0
 #mpirun --map-by node:PE=$SLURM_CPUS_PER_TASK ./applications/single_gpu/gcn --graph $PROJECT/gnn_datasets/reordered/ogbn_products --layers 2 --hunits 256 --epoch 1000 --lr 1e-3 --decay 0  | tee ../results/test_acc_large_graphs_small_lr/sync/products_gcn.txt
@@ -21,6 +33,6 @@ make -j
 
 ## run single-node with async
 #mpirun --map-by node:PE=$SLURM_CPUS_PER_TASK ./applications/async_multi_gpus/gcn --graph $PROJECT/gnn_datasets/reordered/reddit --layers 2 --hunits 256 --epoch 5500 --lr 1e-3 --decay 0  --part hybrid | tee ../results/test_acc_large_graphs_small_lr/async/reddit_gcn.txt
-mpirun --map-by node:PE=$SLURM_CPUS_PER_TASK ./applications/async_multi_gpus/gcn --graph $PROJECT/gnn_datasets/reordered/ogbn_products --layers 3 --hunits 256 --epoch 1000 --lr 1e-3 --decay 0 --part hybrid 
+#mpirun --map-by node:PE=$SLURM_CPUS_PER_TASK ./applications/async_multi_gpus/gcn --graph $PROJECT/gnn_datasets/reordered/ogbn_products --layers 3 --hunits 256 --epoch 1000 --lr 1e-3 --decay 0 --part hybrid 
 #mpirun --map-by node:PE=$SLURM_CPUS_PER_TASK ./applications/async_multi_gpus/gcn --graph $PROJECT/gnn_datasets/reordered/ogbn_arxiv --layers 2 --hunits 256 --epoch 5500 --lr 1e-3 --decay 0 --part hybrid  | tee ../results/test_acc_large_graphs_small_lr/async/arxiv_gcn.txt
 #
