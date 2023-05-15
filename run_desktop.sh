565f1566c119f5cda2492739eb9161069fc7905f
Amadeus Chan <greenclouds@foxmail.com>
2023-05-15 19:10:52 -0400

    start modifying the code incrementally to avoid the previous bugs

--- run_desktop.sh
+++ run_desktop.sh
@@ -9,19 +9,16 @@ make -j
 num_layers=8
 hunits=32
 lr=3e-3
-graph=reddit
+graph=ogbn_arxiv
 epoch=3000
 decay=1e-5
 chunks=16
 dropout=0.5
 seed=5
-model=graphsage
+model=gcn
 eval_freq=10
 
-mpirun -n 1 -N 1 ./applications/async_multi_gpus/$model --graph /home/amadeus/ssd512/gnn_datasets/reordered/$graph --layers $num_layers --hunits $hunits --epoch $epoch --lr $lr --decay $decay --part model --chunks $chunks --weight_file /tmp/saved_weights_pipe --dropout $dropout --seed $seed --eval_freq $eval_freq 
-
-model=gcnii
-mpirun -n 1 -N 1 ./applications/async_multi_gpus/$model --graph /home/amadeus/ssd512/gnn_datasets/reordered/$graph --layers $num_layers --hunits $hunits --epoch $epoch --lr $lr --decay $decay --part model --chunks $chunks --weight_file /tmp/saved_weights_pipe --dropout $dropout --seed $seed --eval_freq $eval_freq 
+mpirun -n 4 -N 4 ./applications/async_multi_gpus/$model --graph /home/amadeus/ssd512/gnn_datasets/reordered/$graph --layers $num_layers --hunits $hunits --epoch $epoch --lr $lr --decay $decay --part model --chunks $chunks --weight_file /tmp/saved_weights_pipe --dropout $dropout --seed $seed --eval_freq $eval_freq 
 
 #cd /home/amadeus/ssd512/baseline/Mithril_MultiGPU/build
 #./applications/single_gpu/gcn_inference --graph /home/amadeus/ssd512/gnn_datasets/reordered/$graph --layers $num_layers --hunits $hunits --weight_file /tmp/saved_weights_pipe
