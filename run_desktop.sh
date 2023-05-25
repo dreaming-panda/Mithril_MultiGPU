796e9a03485362ce5f6a5c12ae154dca83f573b5
Amadeus Chan <greenclouds@foxmail.com>
2023-05-25 19:23:59 -0400

    going to imlement the cpp part of the graph processor

--- run_desktop.sh
+++ run_desktop.sh
@@ -6,18 +6,18 @@ nvidia-smi
 cd build
 make -j
 
-num_layers=4
+num_layers=32
 hunits=64
 lr=1e-3
-graph=ogbn_arxiv
-epoch=5000
+graph=citeseer
+epoch=500
 decay=1e-5
 chunks=8
 dropout=0.5
 seed=5
 model=gcnii
-eval_freq=-1
-exact_inference=1
+eval_freq=10
+exact_inference=0
 num_dp_ways=1
 
 mpirun -n 1 -N 1 ./applications/async_multi_gpus/$model --graph /home/amadeus/ssd512/gnn_datasets/reordered/$graph --layers $num_layers --hunits $hunits --epoch $epoch --lr $lr --decay $decay --part model --chunks $chunks --weight_file /tmp/saved_weights_pipe --dropout $dropout --seed $seed --eval_freq $eval_freq --exact_inference $exact_inference --num_dp_ways $num_dp_ways
---
+++ scripts/graph_partitioner/process_graph.hpp
@@ -0,0 +1,7 @@
+#ifndef PROCESS_GRAPH_HPP
+#define PROCESS_GRAPH_HPP
+
+class GraphProcessor {
+};
+
+#endif
