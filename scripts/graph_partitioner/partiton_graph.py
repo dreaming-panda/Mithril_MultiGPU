73c91df730ae1277cbe8c17b4d95ac220d5d205f
Amadeus Chan <greenclouds@foxmail.com>
2023-05-25 19:03:39 -0400

    implemented the wrapper of the pymetis

--- scripts/graph_partitioner/partiton_graph.py
+++ scripts/graph_partitioner/partiton_graph.py
@@ -1,3 +1,51 @@
-import metis
+import numpy as np
+import pymetis
+import os
+
+def load_graph():
+    with open("./tmp/graph.txt", "r") as f:
+        line = f.readline()
+        assert(line != None)
+        num_vertices = int(line.strip().split(" ")[0])
+        print("Going to load the graph with %s vertices" % (num_vertices))
+        graph = []
+        for i in range(num_vertices):
+            line = f.readline()
+            assert(line != None and len(line) > 0)
+            line = line.strip().split(" ")
+            nbrs = [int(i) for i in line]
+            print(nbrs)
+            graph.append(np.array(nbrs))
+            #if (i + 1) % (num_vertices / 100) == 0:
+            #    print("\tLoading progress %.2f", (i + 1) * 1. / num_vertices)
+        return graph
+
+def partition_graph(graph, num_parts):
+    print("Partitioning the graph...")
+    n_cuts, membership = pymetis.part_graph(num_parts, adjacency=graph)
+    return membership
+
+def dump_parts(membership, num_parts):
+    print("Dumping the partitions...")
+    part_file = "./tmp/%s_parts.txt" % (num_parts)
+    with open(part_file, "w") as f:
+        for i in range(membership):
+            f.write(str(i) + "\n")
 
 if __name__ == "__main__":
+    print("Going to partition the graph with METIS...\n")
+    graph = load_graph()
+
+    num_partitions = []
+    with open("./tmp/num_partitions.txt", "r") as f:
+        line = f.readline()
+        if line == None or len(line) == 0:
+            break
+        num_partitions.append(int(line.strip()))
+
+    for num_parts in num_partitions:
+        print("\n\n\nNumPartitions = %s" % (num_parts))
+        membership = partiton_graph(graph, num_parts)
+        dump_parts(membership, num_parts)
+
+
