1fa743833103aaba44eefdf61f8a85427267cf52
Amadeus Chan <greenclouds@foxmail.com>
2023-08-01 16:23:49 -0400

    added new results

--- run_alchem.sh
+++ run_alchem.sh
@@ -16,20 +16,20 @@ hunits=100
 lr=1e-3
 graph=reddit
 epoch=5000
-decay=0
+decay=1e-5
 dropout=0.5
 #seed=5
-model=gcnii
+model=gcn
 eval_freq=-1
 enable_compression=0
 
 #chunks=$num_gpus
 #num_dp_ways=$num_gpus
 
-chunks=$((num_gpus*4*16))
-num_dp_ways=1
+chunks=$((num_gpus*4))
+num_dp_ways=2
 
-exact_inference=1
+exact_inference=0
 seed=1
 
 mpirun -n $num_gpus --map-by node:PE=8 --host gnerv1:4,gnerv2:4,gnerv6:4 ./applications/async_multi_gpus/$model --graph $dataset_path/$graph --layers $num_layers --hunits $hunits --epoch $epoch --lr $lr --decay $decay --part model --chunks $chunks --weight_file /tmp/saved_weights_pipe --dropout $dropout --seed $seed --eval_freq $eval_freq --exact_inference $exact_inference --num_dp_ways $num_dp_ways --enable_compression $enable_compression 
