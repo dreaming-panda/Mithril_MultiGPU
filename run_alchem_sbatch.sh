051eca73da2d4944fa223f41403641932913e5c6
Amadeus Chan <greenclouds@foxmail.com>
2023-09-18 19:33:10 -0400

    check pointed some new results

--- run_alchem_sbatch.sh
+++ run_alchem_sbatch.sh
@@ -2,7 +2,7 @@
 #SBATCH --output=./output.txt
 #SBATCH --time=00:30:00
 #SBATCH --partition=gpu
-#SBATCH --gpus=8
+#SBATCH --gpus=4
 #SBATCH --gpus-per-task=4
 #SBATCH --exclusive
 
@@ -15,18 +15,18 @@ make -j
 #dataset_path=/shared_hdd_storage/jingjichen/gnn_datasets/weighted_shuffled_partitioned_graphs
 dataset_path=/shared_hdd_storage/jingjichen/gnn_datasets/partitioned_graphs
 
-num_gpus=8
-num_layers=32
+num_gpus=1
+num_layers=8
 hunits=100
 lr=1e-3
-graph=yelp
-epoch=100
+graph=reddit
+epoch=50
 decay=0
 dropout=0.5
-model=resgcn
+model=gcn
 eval_freq=-1
 enable_compression=0
-multi_label=1
+multi_label=0
 
 # graph parallel
 chunks=$num_gpus
@@ -41,4 +41,5 @@ seed=1
 
 echo "Running experiments..."
 
-mpirun -n $num_gpus --map-by node:PE=4 --hostfile ../hostfile ./applications/async_multi_gpus/$model --graph $dataset_path/$graph --layers $num_layers --hunits $hunits --epoch $epoch --lr $lr --decay $decay --part model --chunks $chunks --weight_file /tmp/saved_weights_pipe --dropout $dropout --seed $seed --eval_freq $eval_freq --exact_inference $exact_inference --num_dp_ways $num_dp_ways --enable_compression $enable_compression --multi_label $multi_label
+#mpirun -n $num_gpus --map-by node:PE=4 --hostfile ../hostfile ./applications/async_multi_gpus/$model --graph $dataset_path/$graph --layers $num_layers --hunits $hunits --epoch $epoch --lr $lr --decay $decay --part model --chunks $chunks --weight_file /tmp/saved_weights_pipe --dropout $dropout --seed $seed --eval_freq $eval_freq --exact_inference $exact_inference --num_dp_ways $num_dp_ways --enable_compression $enable_compression --multi_label $multi_label
+mpirun -n $num_gpus --map-by node:PE=4 --host gnerv1:4,gnerv8:4 ./applications/async_multi_gpus/$model --graph $dataset_path/$graph --layers $num_layers --hunits $hunits --epoch $epoch --lr $lr --decay $decay --part model --chunks $chunks --weight_file /tmp/saved_weights_pipe --dropout $dropout --seed $seed --eval_freq $eval_freq --exact_inference $exact_inference --num_dp_ways $num_dp_ways --enable_compression $enable_compression --multi_label $multi_label
