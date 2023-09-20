772f5266979b8a2d50bb3ff121a486ac64cde24a
Amadeus Chan <greenclouds@foxmail.com>
2023-09-19 20:03:13 -0400

    check pointed

--- run_alchem_sbatch.sh
+++ run_alchem_sbatch.sh
@@ -12,15 +12,15 @@ mpirun ./gen_hostfile.sh > ./hostfile
 cd build
 make -j
 
-#dataset_path=/shared_hdd_storage/jingjichen/gnn_datasets/weighted_shuffled_partitioned_graphs
-dataset_path=/shared_hdd_storage/jingjichen/gnn_datasets/partitioned_graphs
+dataset_path=/shared_hdd_storage/jingjichen/gnn_datasets/weighted_shuffled_partitioned_graphs
+#dataset_path=/shared_hdd_storage/jingjichen/gnn_datasets/partitioned_graphs
 
 num_gpus=8
-num_layers=32
+num_layers=128
 hunits=100
 lr=1e-3
 graph=ogbn_arxiv
-epoch=100
+epoch=50
 decay=0
 dropout=0.5
 model=gcnii
@@ -28,13 +28,13 @@ eval_freq=-1
 enable_compression=0
 multi_label=0
 
-# graph parallel
-chunks=$num_gpus
-num_dp_ways=$num_gpus
+## graph parallel
+#chunks=$num_gpus
+#num_dp_ways=$num_gpus
 
-## pipeline parallel
-#chunks=$((num_gpus*8))
-#num_dp_ways=1
+# pipeline parallel
+chunks=$((num_gpus*4))
+num_dp_ways=1
 
 exact_inference=1
 seed=1
